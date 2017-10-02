e83aa3011944b0c4bdec3e0ea35f6c176ffca70a
jimhotchkiss <jimbohtochkiss@yahoo.com>
2017-10-02 17:30:52 -0400

    Done.

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,15 @@ class Item < ActiveRecord::Base
   has_many :carts, through: :line_items
   belongs_to :category
 
+  def self.available_items
+    @available_items = []
+    Item.all.each do |available_item|
+      if available_item.inventory > 0
+        @available_items << available_item
+      end
+    end
+    @available_items
+  end
+
 
 end
